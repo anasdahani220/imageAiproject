59f1ffab8330819d87052b440e63c8c64bda65ba
anasdahani220 <anasdahani220@gmail.com>
2025-09-30 12:08:23 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from '@clerk/nextjs';
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <p>Hom</p>
+        <UserButton />
+    </div>
+
+    
+  )
+}
+
+export default Home ;
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,16 @@
+import { model, models, Schema } from "mongoose";
+
+
+const UserSchema = new Schema({
+    clerkId: {type: String , required: true , unique: true} ,
+    email: {type: String , required: true , unique: true} ,
+    userName: {type: String , required: true , unique: true} ,
+    photo: {type: String , required: true} ,
+    firstName: {type: String } ,
+    lastname: {type: String } ,
+    planId: {type: Number , default: 1} ,
+    creditBalance: {type: Number , default: 10} ,
+
+})
+
+export const User = models?.User || model('User', UserSchema) ;
